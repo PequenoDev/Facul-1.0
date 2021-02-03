a85f20bf953603b3317dd4df63eaf8d83dda22ee
José Borges Nascimento Costa <78379768+PequenoDev@users.noreply.github.com>
2021-02-03 18:34:29 -0300

    Atividades sobre funções da materia de Programação de Computadores

---
+++ ProgramacaoDeComputadores/Lista01Funcao/ativ_002.c
@@ -0,0 +1,61 @@
+/*
+2- Escreva um programa em C, que através do uso de funções, solicite o total 
+gasto pelo cliente de uma loja, imprime as opções de pagamento, solicita a 
+opção desejada e imprime o valor total das prestações (se houverem).
+
+1) Opção: a vista com 10% de desconto
+2) Opção: em duas vezes (preço da etiqueta)
+3) Opção: de 3 até 10 vezes com 3% de juros ao mês (somente para compras acima de R$ 100,00). 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void valor_a_pagar(int op, float valor) {
+
+  switch (op)
+  {
+  case 1:
+    printf("O valor a ser pago é de R$ %.2f\n", valor*0.90);
+    break;
+
+  case 2:
+    printf("O valor a ser pago é 2x de R$ %.2f\n",valor/2);
+    break;
+
+  case 3:
+    break;  
+  
+  }
+}
+
+void formas_de_pagamento(float valor) {
+  int op;
+
+  printf("---->Forma de Pagamento:\n"
+         "--->1) A vista com 10%% de desconto\n"
+         "--->2) Em duas vezes (preço da etiqueta)\n"
+         "--->3) De 3 até 10 vezes com 3%% de juros ao mês (somente para compras acima de R$ 100,00)\n");
+
+         scanf("%d", &op);
+
+      valor_a_pagar(op, valor);
+}
+
+int valor_da_compra() {
+  float valor;
+
+  scanf("%f", &valor);
+  printf("O valor da compra foi de %.2f\n", valor);
+
+  formas_de_pagamento(valor);
+}
+
+int main() {
+  setlocale(LC_ALL, "Portuguese");
+
+  valor_da_compra();
+  
+  return 0;
+}
---
+++ ProgramacaoDeComputadores/ativ_001.c
@@ -0,0 +1,25 @@
+//1- Escreva uma função que receba dois números inteiros retorne o menor número;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int menor() {
+  int a, b;
+
+  scanf("%d %d", &a,&b);
+  if(a<b) {
+    printf("O menor número é %d", a);
+  }
+  else{
+    printf("O menor número é %d", b);
+  }
+}
+
+int main() {
+  setlocale(LC_ALL, "Portuguese");
+  
+  menor();
+
+  return 0;
+}
