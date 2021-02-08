55479c99f638374e2b165299dda878566162201a
José Borges Nascimento Costa <78379768+PequenoDev@users.noreply.github.com>
2021-02-08 16:45:46 -0300

    Atualizações

---
+++ EstruturaDeDados/Lista02Matrizes/areaSuperior.c
@@ -0,0 +1,4 @@
+//Leia um caractere maiúsculo, que indica uma operação que deve ser 
+//realizada e uma matriz M[12][12]. Em seguida, calcule e mostre a soma 
+//ou a média considerando somente aqueles elementos que estão na área 
+//superior da matriz, conforme ilustrado abaixo (área verde).
---
+++ EstruturaDeDados/Lista02Matrizes/matrizQuadrada1.c
@@ -0,0 +1,3 @@
+//Escreva um algoritmo que leia um inteiro N (0 ≤ N ≤ 100), 
+//correspondente a ordem de uma matriz M de inteiros, e construa 
+//a matriz de acordo com o exemplo abaixo.
