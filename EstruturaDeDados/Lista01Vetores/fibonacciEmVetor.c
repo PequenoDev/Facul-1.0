9e2a028b6cdae714292771e24847cf9e5e0c9b3b
José Borges Nascimento Costa <78379768+PequenoDev@users.noreply.github.com>
2021-02-10 22:24:11 -0300

    Atualização

--- EstruturaDeDados/Lista01Vetores/fibonacciEmVetor.c
+++ EstruturaDeDados/Lista01Vetores/fibonacciEmVetor.c
@@ -3,3 +3,29 @@
 //primeiros elementos da série de Fibonacci são 0 e 1 e cada próximo 
 //termo é a soma dos 2 anteriores a ele. Todos os valores de Fibonacci 
 //calculados neste problema devem caber em um inteiro de 64 bits sem sinal.
+
+#include<stdio.h>
+int main() {
+
+   long long int n, prim = 0, seg = 1, prox, x;
+   int i,j;
+
+    scanf("%d", &j);
+    for(i=1; i<=j; i++, prim = 0, seg = 1) {
+
+       scanf("%lld",&n);
+       n=n+1;
+       for ( x = 0 ; x < n ; x++ ) {
+          if (x <= 1) {
+            prox = x;
+          } else {
+             prox = prim + seg;
+             prim = seg;
+             seg = prox;
+          }
+        }
+      printf("Fib(%lld) = %lld\n",n-1,prox);
+    }
+
+   return 0;
+}
--- EstruturaDeDados/Lista01Vetores/menorEposicao.c
+++ EstruturaDeDados/Lista01Vetores/menorEposicao.c
@@ -2,3 +2,28 @@
 //de um vetor X[N]. A seguir, leia cada um dos valores de X, 
 //encontre o menor elemento deste vetor e a sua posição dentro 
 //do vetor, mostrando esta informação.
+
+#include <stdio.h>
+
+int main() {
+
+  int i, n, pos, menor;
+
+    scanf("%d", &n);
+    int vetor[n];
+    
+    for(i=0; i<n; i++) {
+      scanf("%d", &vetor[i]);
+      menor=vetor[0];
+    }
+
+    for(i=1; i<n; i++) {
+      if(menor>vetor[i]) {
+        menor=vetor[i];
+        pos=i;
+      }
+    }
+    printf("Menor valor: %d\nPosicao: %d\n", menor, pos);
+
+    return 0;
+}
