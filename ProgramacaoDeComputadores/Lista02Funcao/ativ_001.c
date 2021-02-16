2b8071baac1f54db0b32835f2dc0e1465606aad1
José Borges Nascimento Costa <78379768+PequenoDev@users.noreply.github.com>
2021-02-16 17:17:55 -0300

    novas

--- ProgramacaoDeComputadores/Lista02Funcao/ativ_001.c
+++ ProgramacaoDeComputadores/Lista02Funcao/ativ_001.c
@@ -1,1 +1,19 @@
 //1- Implemente a função strcpy (char *destino, char *origem) usando ponteiros.
+
+#include <stdio.h>
+#include <string.h>
+
+void strcopy(char *str2, char *str1) {
+  
+}
+
+int main() {
+  char str1[50], str2[50];
+ 
+  scanf("%s%s", str1, str2);
+  getchar();
+  printf("%s\n%s", str1, str2);
+
+  strcopy(str1,str2);
+  return 0;
+}
