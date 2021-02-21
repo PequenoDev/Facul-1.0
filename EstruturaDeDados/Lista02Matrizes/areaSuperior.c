f8cc084c6ca6c0a3f44d15b43b3c4380271fc0ba
José Borges Nascimento Costa <78379768+PequenoDev@users.noreply.github.com>
2021-02-21 12:54:14 -0300

    Atualização de atividades

--- EstruturaDeDados/Lista02Matrizes/areaSuperior.c
+++ EstruturaDeDados/Lista02Matrizes/areaSuperior.c
@@ -2,3 +2,36 @@
 //realizada e uma matriz M[12][12]. Em seguida, calcule e mostre a soma 
 //ou a média considerando somente aqueles elementos que estão na área 
 //superior da matriz, conforme ilustrado abaixo (área verde).
+
+#include <stdio.h>
+int main()
+{
+  double x=0.0, M[12][12];
+  char J[2];
+  int i,l,c,y,cont=10,q=1;
+  
+  scanf("%s", &J);
+  for(l=0;l<=11; l++)
+  {
+    for(c=0; c<=11; c++)
+      scanf("%lf", &M[l][c]);
+  }
+  
+  for(y=0; y<=4; y++)
+  {
+    for(i=q; i<=cont; i++) 
+      x+=M[y][i];
+    cont--;
+    q++;
+  }
+  
+  if(J[0]=='S')
+    printf("%.1lf\n",x);
+  else if(J[0]=='M')
+  {
+    x=x/30.0;
+    printf("%.1lf\n",x);
+  }
+  
+return 0;
+}
--- EstruturaDeDados/Lista02Matrizes/matrizQuadrada1.c
+++ EstruturaDeDados/Lista02Matrizes/matrizQuadrada1.c
@@ -1,3 +1,51 @@
 //Escreva um algoritmo que leia um inteiro N (0 ≤ N ≤ 100), 
 //correspondente a ordem de uma matriz M de inteiros, e construa 
 //a matriz de acordo com o exemplo abaixo.
+
+#include <stdio.h>
+
+int main()
+{
+  int j,o,s,e,x,i,y=1,N,p=0,q=0,r;
+  
+  while(1)
+  {
+    scanf("%d", &N);
+    if(N==0) break;
+    else
+    {
+      y=1;p=0;q=0;
+      int ara[N][N];
+      i=N;
+      if(N%2==0)
+        r=N/2;
+      else if(N%2==1)
+        r=(N/2)+1;
+      
+      for(s=1; s<=r; s++)
+      {
+        for(j=p; j<i; j++)
+        {
+          for(o=q; o<i; o++)
+            ara[j][o]=y;
+        }
+        y++;
+        p++; q++; i--;
+      }
+      
+      for(e=0; e<N; e++)
+      {
+        for(x=0; x<N; x++)
+        {
+          if(x==0)
+            printf("%3d",ara[e][x]);
+          else printf(" %3d",ara[e][x]);
+        }
+        printf("\n");
+      }
+        printf("\n");
+    }
+  }
+
+return 0;
+}
